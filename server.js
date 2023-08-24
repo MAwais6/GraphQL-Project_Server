49f0e2e256da41c48f7fb3582301d656398dbe43
MAwais6 <muhammadawais.contact0@gmail.com>
2023-08-24 22:26:49 +0500

    GraphQL | Setup  ✅✔️

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
+// import Schema files 
+const schema = require('./schema/schema');
+
 const app = express();
 
+// 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+// GraphQL
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}));
 
 
 app.listen(port, () => {
