edc543f47616d6b725130c101f69dde3afd69cda
MAwais6 <muhammadawais.contact0@gmail.com>
2023-08-24 22:37:55 +0500

    Sample Data | Added  ✅✔️

---
+++ Data/sampleData.js
@@ -0,0 +1,52 @@
+const clients = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        phone: '555-555-5555'
+    },
+    {
+        id: 2,
+        name: 'Karen Williams',
+        email: 'karen@gmail.com',
+        phone: '444-444-4444'
+    },
+    {
+        id: 3,
+        name: 'Henry Johnson',
+        email: 'henry@gmail.com',
+        phone: '333-333-3333'
+    }
+];
+
+// projects data
+const projects = [
+    {
+        id: 1,
+        clientId: 1,
+        name: 'Business Website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, voluptates.',
+        status: 'active'
+    },
+    {
+        id: 2,
+        clientId: 1,
+        name: 'Shopping Cart',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, voluptates.',
+        status: 'active'
+    },
+    {
+        id: 3,
+        clientId: 2,
+        name: 'Business Website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, voluptates.',
+        status: 'active'
+    },
+    {
+        id: 4,
+        clientId: 3,
+        name: 'Business Website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, voluptates.',
+        status: 'inactive'
+    }
+];
